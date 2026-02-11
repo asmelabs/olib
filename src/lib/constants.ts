fa239b6bb5d241afa8032a283964424c063b5d9d
Mehdi Asadli <asadlimehdi25@gmail.com>
2026-02-11 18:34:10 +0400

    feat: add constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_LIMIT = 100;
+export const DEFAULT_OFFSET = 0;
+export const MAX_LIMIT = 1000;
+
+export const USER_AGENT = "@asmelabs/olib";
+
+export const BASE_URL = "https://openlibrary.org";
+export const COVERS_BASE_URL = "https://covers.openlibrary.org";
+export const DOCS_BASE_URL = `${BASE_URL}/dev/docs/api`;
